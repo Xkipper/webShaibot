17d697aa96313b7c1bb4637ecdd49d3b410deb28
Xkipper <yo.nibiru@gmail.com>
2022-03-15 11:34:02 -0300

    ..

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
 console.clear();
+import express from 'express';
+import { resolve, join } from 'path';
+
+
+const app = express();
+app.use(express.static(join(resolve("./"), 'html')));
+
+// listen for requests :)
+const PORTe : string|number = process.env.PORT || 5000;
+app.listen(PORTe, () => {
+    console.log(`Our app is running on port ${ PORTe }`);
+});
